497dfefe3e236abf6c69345410ed2716c9e6e115
Nathally <thallycoutinho@gmail.com>
2019-12-10 21:19:34 -0300

    TDD BLOG Formulario

---
+++ posts/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from .forms import PedidoForm
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def post(request):
+    return render(request, 'post.html')
+
+def cadastro(request):
+    form = PedidoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        context = {
+            'msg':"Pedido realizado com sucesso!"
+        }
+        return render(request, 'cadastro.html', context)
+    context = {
+        'formulario':form
+    }
+    return render(request, 'cadastro.html', context)
